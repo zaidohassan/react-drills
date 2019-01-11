9a6ae7efd70149ae101bf663f0b734487300dc77
Zaid Hassan <zaidhassan@Zaids-MacBook-Pro.local>
2019-01-11 15:09:00 -0600

    updated solution

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,21 +1,50 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+    constructor() {
+      super();
+  
+      this.state = {
+        filteredFood: '',
+        foods: [
+          "spaghetti",
+          "ice cream",
+          "sushi",
+          "bologna",
+          "cheese"
+        ]
+      }
+    }
+  
+
+update(val){
+    this.setState({filteredFood: val})
+}
+  //user inputs name to find
+  // input automatically filters through array
+  // word will show on bottom of input screen
+
   render() {
+    let foodFilter = this.state.foods.filter((e,i) => {
+    return e.includes(this.state.filteredFood) 
+    }).map((e,i) => {
+      return <h2 key={ i }>{ e }</h2>
+    })
+
+    console.log(foodFilter);
+    
+  
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <input onChange={((e) => this.update(e.target.value))} type= 'text' />
+        {foodFilter}
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        
+        
     );
   }
 }
+
 
 export default App;
