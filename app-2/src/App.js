8a489aa547a20711fe6dc0215a152c1913a17c7c
Zaid Hassan <zaidhassan@Zaids-MacBook-Pro.local>
2019-01-11 15:10:22 -0600

    upated soltuion

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -3,16 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+  super()
+  this.state = {
+    arr: ["Hi", "Hello", "Hola", "Salam"]
+  }
+  }
+  
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p> {this.state.arr[0]} </p>
+        <p> {this.state.arr[1]} </p>
+        <p> {this.state.arr[2]} </p>
+        <p> {this.state.arr[3]} </p>
+
       </div>
     );
   }
