d91f423ac3126ce9af8a046bbdeb5971ea3bfceb
Zaid Hassan <zaidhassan@Zaids-MacBook-Pro.local>
2019-01-14 08:19:01 -0600

    updated solution

--- app-6/src/App.js
+++ app-6/src/App.js
@@ -1,21 +1,78 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Todolist from './Component/todolist'; 
 import './App.css';
 
+
+
 class App extends Component {
+  constructor(){
+    super()
+    this.state ={
+        str: '',
+        arr: []
+    }
+
+  
+    this.handleTask = this.handleTask.bind(this);
+  }
+
+  
+addList(val){
+  this.setState({str: val})
+}
+
+handleTask(){
+    let newArr = this.state.arr.map((e,i) => e)
+    newArr.push(this.state.str)
+    this.setState({arr: newArr})
+}
+//   this.setState( {
+//   list: [...this.state.arr, this.state.str ], 
+//   str: '' 
+// });
+// }
+
+
+
+// user inputs data
+// user clicks button
+// when i click add or button, it will take that string and it to the list of array 
+// then display each string on one line
+
+
   render() {
+    
+    let arr1 = this.state.arr.map((e,i) => {
+        return ( 
+        <Todolist key={i} task={ e }/>
+    )
+  })
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <h1> My to-do list:</h1>
+    <div> 
+    <input 
+    value= {this.state.str} 
+    placeholder= "Enter New Task"
+    onChange={(e) => this.addList(e.target.value)} />
+    
+    <button onClick={this.handleTask}> Add </button> 
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+ 
+    <br />
+
+    { arr1 }
+
+       </div> 
+      
+       
     );
   }
 }
 
 export default App;
+
+  
+
+      
---
+++ app-6/src/Component/todolist.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function Todolist (props) {
+      return (
+        <p> {props.task}</p>
+      )
+    }
+
